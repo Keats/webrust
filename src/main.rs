d41d3148fdfc9d74e32994551b2b136e81e325a7
Vincent Prouillet <vincent@wearewizards.io>
2015-09-29 17:37:13 +0100

    Add basic POST with json handling

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ extern crate rustful;
 extern crate serde;
 extern crate serde_json;
 
+use rustful::Method::{Get, Post};
+use std::io::{BufReader, Read};
 use std::error::Error;
-use rustful::{Server, Context, Response, TreeRouter};
+use rustful::{Server, Context, Response, Router, TreeRouter};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Password {
     id: i32,
     encrypted: String,
@@ -23,18 +25,27 @@ fn get_passwords(context: Context, response: Response) {
     response.send(payload);
 }
 
+fn create_password(context: Context, response: Response) {
+    let mut buffer = String::new();
+    BufReader::new(context.body).read_to_string(&mut buffer).unwrap();
+    let password: Password = serde_json::from_str(&buffer).unwrap();
+    let payload = serde_json::to_string(&password).unwrap();
+
+    response.send(payload);
+}
+
 fn main() {
-    let routes = insert_routes!{
-        TreeRouter::new() => {
-            "passwords" => {
-                Get: get_passwords,
-            }
-        }
-    };
+    let mut router = TreeRouter::new();
+    // needed to avoid
+    // expected `fn(rustful::context::Context<'_, '_, '_>, rustful::response::Response<'_, '_>) {get_passwords}`,
+    // found `fn(rustful::context::Context<'_, '_, '_>, rustful::response::Response<'_, '_>) {create_password}
+    found `fn(rustful::context::Context<'_, '_, '_>, rustful::response::Response<'_, '_>) {create_password}`
+    router.insert(Get, &"passwords", get_passwords as fn(Context, Response));
+    router.insert(Post, &"passwords", create_password as fn(Context, Response));
 
     let server_result = Server {
         host: 8080.into(),
-        handlers: routes,
+        handlers: router,
         ..Server::default()
     }.run();
 
